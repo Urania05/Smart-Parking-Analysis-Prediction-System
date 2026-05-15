8c96e98b70b246ae9cdf4d5b3f30c2e932b1f8dd
Kemal Özkan <154462373+Kemalzkn@users.noreply.github.com>
2026-05-15 11:54:13 +0300

    Remove Docker comment from database connection
    Removed comment about connecting to the database in Docker.

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Docker üzerindeki veri tabanımıza bağlanıyoruz
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@localhost:5432/akilli_otopark"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
